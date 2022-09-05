bf4f68dbf15b7104fb28ea81b56fdfd33c6700c7
MasKatx <tky2102137@stu.o-hara.ac.jp>
2022-09-05 16:02:33 +0900

    [add]パンくずリストの追加、及びそれに伴うDefaultの変更
    BreadCrumb.tsxを追加し、Default.tsx、TagList.tsxを変更しました。尚、Headerはhoverを追加しました。

--- src/components/template/Default/Default.tsx
+++ src/components/template/Default/Default.tsx
@@ -1,17 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import Header from "components/organisms/Header/Header";
 import Footer from "components/organisms/Footer/Footer";
+import BreadCrumbs from "components/organisms/BreadCrumbs/BreadCrumbs";
 import { ReactNode } from 'react';
 
 type DefaultType = {
 	readonly children?: ReactNode;
+    readonly contents?: string[][];
 };
 
 const Default = (props: DefaultType) => {
 	return (
         <>
 			<Header />
-            <main>{props.children}</main>
+            <main>
+                <BreadCrumbs
+                    contents={props.contents}
+                />
+                {props.children}
+            </main>
             <Footer />
         </>
 	);
