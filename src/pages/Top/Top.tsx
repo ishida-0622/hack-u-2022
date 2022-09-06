c775f402685edefa8d6f65c3a4647c7588fa8ff3
kentaro ishida <ken.babuo@gmail.com>
2022-09-07 02:01:43 +0900

    update: 導線の追加(未完成)

--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,13 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import Textarea from "components/atoms/Textarea/Textarea";
-import { useState } from "react";
-import Button from "components/atoms/Button/Button";
-import Text from "components/atoms/Text/Text";
 import Default from "components/template/Default/Default";
+import Link from "components/atoms/Link/Link";
 
 const Top = () => {
-    const [txt, setTxt] = useState("");
     return (
         <Default>
             <div
@@ -15,37 +11,11 @@ const Top = () => {
                     textAlign: "center",
                 })}
             >
-                <h1>Top</h1>
-                <h2>test</h2>
-                <Textarea
-                    css={style}
-                    value={txt}
-                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
-                        setTxt(e.target.value)
-                    }
-                ></Textarea>
-                <br />
-                <Button
-                    css={buttonCss}
-                    value="click"
-                    onClick={() => setTxt("")}
-                    children={"reset"}
-                />
-                <br />
-                <Text>{txt}</Text>
+                <Link href="/post-create">布教しに行く</Link>
+                <Link href="/foo">布教されに行く</Link>
             </div>
         </Default>
     );
 };
-
-const style = css({
-    textAlign: "center",
-    color: "#f00",
-    backgroundColor: "#aaa",
-});
-
-const buttonCss = css({
-    backgroundColor: "#0f0",
-});
 
 export default Top;
