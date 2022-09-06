11a76679f777c33379df421cbd440c21498bfa70
kentaro ishida <ken.babuo@gmail.com>
2022-09-06 12:10:25 +0900

    update: 再レンダリング時に画面が一瞬白飛びするのでuseLayoutEffectに変更

--- src/hooks/useLoginUser.tsx
+++ src/hooks/useLoginUser.tsx
@@ -1,11 +1,11 @@
 import { User } from "firebase/auth";
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import getLoginUser from "utils/getLoginUser";
 
 const useLoginUser = (): [User | null, boolean] => {
     const [user, setUser] = useState<User | null>(null);
     const [load, setLoad] = useState<boolean>(false);
-    useEffect(() => {
+    useLayoutEffect(() => {
         const f = async () => {
             setUser(await getLoginUser());
             setLoad(true);
