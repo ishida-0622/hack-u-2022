4b4ed0575192204a54c15e2d0c02bfacffe83422
kentaro ishida <ken.babuo@gmail.com>
2022-09-09 20:09:42 +0900

    clean: 不要なインポートの削除

--- src/pages/Mypage/Mypage.tsx
+++ src/pages/Mypage/Mypage.tsx
@@ -3,27 +3,25 @@ import { css } from "@emotion/react";
 import useUserData from "hooks/useUserData";
 import Image from "components/atoms/Image/Image";
 import Default from "components/template/Default/Default";
-import NowLoading from "components/atoms/NowLoading/NowLoading";
-import { Navigate } from "react-router-dom";
-import useLoginUser from "hooks/useLoginUser";
+// import NowLoading from "components/atoms/NowLoading/NowLoading";
+// import { Navigate } from "react-router-dom";
+// import useLoginUser from "hooks/useLoginUser";
 
 const Mypage = () => {
     document.title = "マイページ";
-    const [user, load] = useLoginUser();
+    // const [user, load] = useLoginUser();
     const [userData] = useUserData();
     return (
         <Default>
-            <h2>
-                登録情報
-            </h2>
+            <h2>登録情報</h2>
 
             <div
                 css={css({
-                        display: "block",
-                        margin: "0 auto",
-                        width: "200px",
-                        height: "100px",
-                    })}
+                    display: "block",
+                    margin: "0 auto",
+                    width: "200px",
+                    height: "100px",
+                })}
             >
                 <div
                     css={css({
@@ -38,10 +36,9 @@ const Mypage = () => {
                         width={100}
                         height={100}
                     />
-                        {userData?.name}
+                    {userData?.name}
                 </div>
             </div>
-
         </Default>
     );
 };
