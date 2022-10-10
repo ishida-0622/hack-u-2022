22823ce86a49d4fe1819ccd67e040d0509b0e61f
kentaro ishida <ken.babuo@gmail.com>
2022-10-10 15:57:29 +0900

    add: フォロー/フォロー解除ボタン

---
+++ src/components/organisms/FollowButton/index.tsx
@@ -0,0 +1,68 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState } from "react";
+import Button from "components/atoms/Button";
+
+/**
+ * フォロー/フォロー解除ボタン
+ * @param props functionActivatedWhenFollow -> フォロー時に実行する関数, functionActivatedWhenUnfollow -> フォロー解除時に実行する関数
+ * @returns フォローボタン
+ */
+const FollowButton = (props: {
+    className?: string;
+    tag: string;
+    isFollow: boolean;
+    functionActivatedWhenFollow?: (tag: string) => void;
+    functionActivatedWhenUnfollow?: (tag: string) => void;
+}) => {
+    const [isFollow, setIsFollow] = useState(props.isFollow);
+    const [text, setText] = useState(isFollow ? "フォロー中" : "フォロー");
+    return (
+        <Button
+            className={props.className}
+            css={[buttonStyle, hoverStyle(isFollow)]}
+            onClick={() => {
+                if (isFollow) {
+                    if (
+                        window.confirm("フォローを解除してもよろしいですか？")
+                    ) {
+                        if (props.functionActivatedWhenUnfollow) {
+                            props.functionActivatedWhenUnfollow(props.tag);
+                        }
+                    }
+                } else {
+                    if (props.functionActivatedWhenFollow) {
+                        props.functionActivatedWhenFollow(props.tag);
+                    }
+                }
+                setText(isFollow ? "フォロー" : "フォロー解除");
+                setIsFollow(!isFollow);
+            }}
+            onMouseOver={() => (isFollow ? setText("フォロー解除") : {})}
+            onMouseOut={() => setText(isFollow ? "フォロー中" : "フォロー")}
+        >
+            {text}
+        </Button>
+    );
+};
+
+const buttonStyle = css({
+    width: "6rem",
+    margin: "0 0 0 auto",
+    border: "none",
+    borderRadius: 5,
+    backgroundColor: "#1da1f2",
+    color: "white",
+    ":hover": {
+        cursor: "pointer",
+    },
+});
+
+const hoverStyle = (isFollow: boolean) =>
+    css({
+        ":hover": {
+            backgroundColor: isFollow ? "red" : "#1877f2",
+        },
+    });
+
+export default FollowButton;
