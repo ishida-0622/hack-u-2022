5ecaddfe6ff0c2c2848facda40dafa764a0ca298
kentaro ishida <ken.babuo@gmail.com>
2022-10-10 11:39:57 +0900

    add: ログイン確認と未ログイン時に/loginに飛ばす処理

---
+++ src/components/organisms/LoginCheck/index.tsx
@@ -0,0 +1,21 @@
+import useLoginUser from "hooks/useLoginUser";
+import NowLoading from "components/atoms/NowLoading/NowLoading";
+import { Navigate } from "react-router-dom";
+import { ReactNode } from "react";
+
+const LoginCheck = (props: { children: ReactNode }) => {
+    const [user, load] = useLoginUser();
+    return (
+        <>
+            {!load ? (
+                <NowLoading />
+            ) : !user ? (
+                <Navigate to={"/login"} />
+            ) : (
+                <>{props.children}</>
+            )}
+        </>
+    );
+};
+
+export default LoginCheck;
