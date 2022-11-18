d5f02df51417b8bac3ef15d3719b22598d32dd31
kentaro ishida <ken.babuo@gmail.com>
2022-11-19 02:12:42 +0900

    update: get関数、エラーもreturnするように変更、例外処理の追加

--- src/hooks/useAllTags.tsx
+++ src/hooks/useAllTags.tsx
@@ -1,24 +1,34 @@
-import { useState, useLayoutEffect } from "react";
+import { useState, useCallback } from "react";
 import { getDoc, doc } from "firebase/firestore";
 import { db } from "firebaseConfig";
 import { allTagConverter } from "types/allTagsType";
+import { FirebaseError } from "firebase/app";
 
-const useAllTags = (): [string[], boolean] => {
-    const [tags, setTags] = useState<string[]>([]);
+const useAllTags = () => {
+    const [allTags, setAllTags] = useState<string[]>([]);
     const [load, setLoad] = useState(false);
-    useLayoutEffect(() => {
-        const getAllTags = async () => {
+    const [error, setError] = useState("");
+    const getAllTags = useCallback(async () => {
+        try {
             const res = (
                 await getDoc(
                     doc(db, "all_tags/all").withConverter(allTagConverter)
                 )
-            ).data()!;
-            setTags(res.tags);
+            ).data();
+            setAllTags(res ? res.tags : []);
             setLoad(true);
-        };
-        getAllTags();
+        } catch (e) {
+            if (e instanceof FirebaseError) {
+                setError(e.code);
+            } else if (e instanceof Error) {
+                setError(e.message);
+            } else {
+                setError("Error");
+            }
+            setLoad(true);
+        }
     }, []);
-    return [tags, load];
+    return { allTags, getAllTags, load, error };
 };
 
 export default useAllTags;
