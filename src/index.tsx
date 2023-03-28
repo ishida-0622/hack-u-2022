7ae268791c068f735c2c0ee8bc63da3ae15440dc
kentaro ishida <ken.babuo@gmail.com>
2023-03-28 11:39:55 +0900

    clean: 不要部分の削除、コメントアウトの削除、

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
 import "index.css";
-import reportWebVitals from "reportWebVitals";
 import NotFound from "pages/NotFound";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "pages/Signup";
 import Login from "pages/Login";
-import TagList from "pages/TagList";
 import Top from "pages/Top";
 import RecommendedTags from "pages/RecommendedTags";
 import RecommendedMessage from "pages/RecommendedMessage";
@@ -14,9 +11,9 @@ import PostCreate from "pages/PostCreate";
 import MyPostList from "pages/MyPostList";
 import PostEdit from "pages/PostEdit";
 import FollowTag from "pages/FollowTag";
-import Follow from "pages/Follow";
+import Follow from "pages/Tags";
 import Mypage from "pages/Mypage";
-// import EditData from "pages/EditData/EditData";
+import Tags from "pages/Tags";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -29,8 +26,7 @@ root.render(
             <Route path="/login" element={<Login />}></Route>
             <Route path="/signup" element={<Signup />}></Route>
             <Route path="/mypage" element={<Mypage />}></Route>
-            {/* <Route path="/userdata-edit" element={<EditData />}></Route> */}
-            <Route path="/tags" element={<TagList />}></Route>
+            <Route path="/tags" element={<Tags />}></Route>
             <Route path="/follows" element={<FollowTag />}></Route>
             <Route
                 path="/recommended-tags"
@@ -49,8 +45,3 @@ root.render(
     </BrowserRouter>
     // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
