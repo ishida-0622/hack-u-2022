db1ddfab895c18ee0a66d39efa043ad2f1c443dc
kentaro ishida <ken.babuo@gmail.com>
2023-03-28 10:59:21 +0900

    clean: 不要な空タグの削除、引数をreadonlyに変更

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -3,61 +3,62 @@ import { Link } from "react-router-dom";
 import { css } from "@emotion/react";
 import logout from "utils/logout";
 
-const Header = ({ notNav = false }: { notNav?: boolean }) => {
+const Header = ({ notNav = false }: { readonly notNav?: boolean }) => {
     return (
-        <>
-            <header css={HeaderMain}>
-                <h1 css={HeaderH1}>
-                    <Link to="/" css={HeaderLink}>
-                        WAIFU sharing
-                    </Link>
-                </h1>
-                {notNav ? (
-                    <></>
-                ) : (
-                    <nav css={HeaderNav}>
-                        <ul css={HeaderUl}>
-                            <li css={HeaderLi}>
-                                <Link to="/post-create" css={HeaderLink}>
-                                    布教する
-                                </Link>
-                            </li>
-                            <li css={HeaderLi}>
-                                <Link to="/recommended-tags" css={HeaderLink}>
-                                    布教される
-                                </Link>
-                            </li>
-                            <li css={HeaderLi}>
-                                <Link
-                                    to="#"
-                                    css={HeaderLink}
-                                    onClick={() => logout()}
-                                >
-                                    Logout
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
-                )}
-            </header>
-        </>
+        <header css={HeaderMain}>
+            <h1 css={HeaderH1}>
+                <Link to="/" css={HeaderLink}>
+                    WAIFU sharing
+                </Link>
+            </h1>
+            {notNav ? (
+                <></>
+            ) : (
+                <nav css={HeaderNav}>
+                    <ul css={HeaderUl}>
+                        <li css={HeaderLi}>
+                            <Link to="/post-create" css={HeaderLink}>
+                                布教する
+                            </Link>
+                        </li>
+                        <li css={HeaderLi}>
+                            <Link to="/recommended-tags" css={HeaderLink}>
+                                布教される
+                            </Link>
+                        </li>
+                        <li css={HeaderLi}>
+                            <Link
+                                to="#"
+                                css={HeaderLink}
+                                onClick={() => logout()}
+                            >
+                                Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </header>
     );
 };
 
 const HeaderMain = css({
-    padding: "30px 4% 10px 0px",
+    // padding: "30px 4% 10px 0px",
     position: "fixed",
     top: 0,
     width: "100%",
-    backgroundColor: "#fff",
+    height: "12%",
+    // backgroundColor: "#fff",
+    backgroundColor: "#e6e6e6",
     display: "flex",
     alignItems: "center",
+    zIndex: 1,
 });
 
 const HeaderH1 = css({
     margin: 0,
     paddingLeft: "3%",
-    fontSize: "40px",
+    // fontSize: "40px",
 });
 
 const HeaderNav = css({
