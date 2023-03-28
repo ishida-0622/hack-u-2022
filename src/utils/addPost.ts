066b56606ecce90e3344fa6cace644837c64cfb5
kentaro ishida <ken.babuo@gmail.com>
2023-03-28 10:56:41 +0900

    clean: コメントの追加

--- src/utils/addPost.ts
+++ src/utils/addPost.ts
@@ -3,6 +3,12 @@ import { db } from "firebaseConfig";
 import { User } from "firebase/auth";
 import { postType } from "types/postType";
 
+/**
+ * 投稿を追加する
+ * @param user ログイン中のユーザー
+ * @param post 投稿内容
+ * @returns DBに投稿が追加されたら解決されるPromise
+ */
 const addPost = (user: User, post: postType): Promise<void> => {
     return new Promise((resolve, reject) => {
         if (!user) reject("user is undefined");
